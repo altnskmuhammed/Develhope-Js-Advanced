808479374baa144da3ab058d6bbb66bdc3082935
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-10-12 22:37:15 +0300

    Create exercise8.js

---
+++ exercise8/exercise8.js
@@ -0,0 +1,14 @@
+function isAdult(age) {
+  return age >= 18;
+}
+
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+const { id, firstName, lastName, age } = person;
+
+console.log(isAdult(age));
