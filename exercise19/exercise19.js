b7ea98fbe7ea4ddb92e1e84223005687b24a0e72
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-10-19 08:02:58 +0300

    Create exercise19.js

---
+++ exercise19/exercise19.js
@@ -0,0 +1,7 @@
+function printAsyncName(callback, name) {
+  let id = setInterval(callback, 1000);
+  setTimeout(() => clearInterval(id), 2030);
+  setTimeout(() => console.log(name), 2030);
+}
+
+printAsyncName(() => console.log("Hello"), "Muhammed");
