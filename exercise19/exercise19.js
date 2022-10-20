74f8a9d5466e5ce8a6c7dea418fa6f219c88b9df
Muhammed Altunışık <altnskmuh23@gmail.com>
2022-10-20 12:14:21 +0300

    Update exercise19.js

--- exercise19/exercise19.js
+++ exercise19/exercise19.js
@@ -1,6 +1,6 @@
 function printAsyncName(callback, name) {
   let id = setInterval(callback, 1000);
-  setTimeout(() => clearInterval(id), 2030);
+  setTimeout(() => clearInterval(id), 3000);
   setTimeout(() => console.log(name), 2030);
 }
 
